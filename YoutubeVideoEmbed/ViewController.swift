06f9e7a04bc826f639c388a5f1110b807cff907f
cagdaseksi <cagdas.eksi@hotmail.com>
2019-01-06 13:10:03 +0300

    initial commit.

--- YoutubeVideoEmbed/ViewController.swift
+++ YoutubeVideoEmbed/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getVideo(videoCode: "Ou3bl5VQ3OA")
     }
 
+    func getVideo(videoCode:String)
+    {
+        let url = URL(string: "https://www.youtube.com/embed/\(videoCode)")
+        webView.loadRequest(URLRequest(url: url!))
+    }
 
 }
 
